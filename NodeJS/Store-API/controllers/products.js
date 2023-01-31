f5163ee8bf800a8fec60b7e2c4302349f89a66e9
Azar Gurbanov <103862912+azar-g@users.noreply.github.com>
2023-01-31 16:00:48 +0400

    Query operators added

--- NodeJS/Store-API/controllers/products.js
+++ NodeJS/Store-API/controllers/products.js
@@ -1,26 +1,44 @@
 const mongoose = require("mongoose");
 const Product = require("../models/product");
 
-const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query);
+const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find({ price: { $gte: 30, $lt: 100 } }).sort(
+    "price"
+  );
   res.status(200).json({ products, number: products.length });
 };
 
-const getAllProductsStatic = async (req, res) => {
+const getAllProducts = async (req, res) => {
   console.log(req.query);
+  const { name, company, featured, sort, field } = req.query;
 
   const queryObject = {};
 
-  const { name } = req.query;
   if (name) queryObject.name = { $regex: name, $options: "i" };
 
-  const { company } = req.query;
   if (company) queryObject.company = company;
 
-  const { featured } = req.query;
   if (featured) queryObject.featured = req.query.featured;
 
-  const products = await Product.find(queryObject);
+  let results = Product.find(queryObject);
+
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    console.log(sortList);
+    results = results.sort(sortList);
+  } else results = results.sort("price");
+
+  if (field) {
+    const fieldList = field.split(",").join(" ");
+    results = results.select(fieldList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 5;
+  const skip = (page - 1) * limit;
+  results = results.skip(skip).limit(limit);
+
+  const products = await results;
   res.status(200).json({ products, number: products.length });
 };
 
