80377e5131abc7cf89be41b9da7730bc6fcd47e4
Azar Gurbanov <103862912+azar-g@users.noreply.github.com>
2023-02-02 07:45:17 +0400

    JWT added

---
+++ NodeJS/JWT/controllers/main.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const CustomAPIError = require("../errors/custom-error");
+
+const login = (req, res, next) => {
+  console.log(req.body);
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide your username and password", 400);
+  }
+  const id = new Date().getDate();
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  res.status(200).json({ msg: "user created", token });
+};
+
+const dashboard = (req, res, next) => {
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    msg: "Hello",
+    secret: `Here is your authorised data, your lucky numbere is ${luckyNumber} `,
+  });
+};
+
+module.exports = { login, dashboard };
